d7e1b01568d14b0a92016c694eebfef67f37feba
ulia012008 <ulia012008@gmail.com>
2024-12-03 05:15:05 +0100

    modal

--- scripts.js
+++ scripts.js
@@ -1,9 +1,20 @@
-
 const sendButton = document.querySelector('.send-btn');
 const planeIcon = document.querySelector('.plane-icon');
+
 sendButton.addEventListener('click', () => {
-planeIcon.classList.add('plane-fly');
-setTimeout(() => {
-planeIcon.classList.remove('plane-fly');
-  }, 1000);
+  // Перевіряємо, чи анімація відльоту вже не активна
+  if (!planeIcon.classList.contains('plane-fly')) {
+    planeIcon.classList.add('plane-fly');
+    
+    // Після завершення анімації відльоту, додаємо анімацію повернення
+    setTimeout(() => {
+      planeIcon.classList.remove('plane-fly');
+      planeIcon.classList.add('plane-return');
+      
+      // Видаляємо клас повернення після його завершення
+      setTimeout(() => {
+        planeIcon.classList.remove('plane-return');
+      }, 1000); // Тривалість анімації повернення
+    }, 1000); // Тривалість анімації відльоту
+  }
 });
